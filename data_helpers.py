9513f0fb48ce7ea16930ee162f81d937acf537ce
ssy05468 <36706193+ssy05468@users.noreply.github.com>
2018-12-15 19:45:09 +0900

    Update data_helpers.py

--- data_helpers.py
+++ data_helpers.py
@@ -56,8 +56,11 @@ def load_data_and_labels(positive_data_file, negative_data_file):
     negative_examples = [s.strip() for s in negative_examples]
     # Split by words
     x_text = positive_examples + negative_examples
+    #print(x_text)
+    
     x_text = [tokenize(clean_str(sent)) for sent in x_text]
-
+    #print(x_text)
+    
     positive_labels = [[0, 1] for _ in positive_examples]
     negative_labels = [[1, 0] for _ in negative_examples]
     y = np.concatenate([positive_labels, negative_labels], 0)
