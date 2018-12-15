944d96be3a84788ef1470afb3cb9148a383d0d82
Dimen <a4gkyum@naver.com>
2018-12-15 15:27:28 +0900

    Minor fix

--- text_cnn.py
+++ text_cnn.py
@@ -1,25 +1,18 @@
 import tensorflow as tf
 import numpy as np
-import os
-
-os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"] = "4"
 
 
-class TextCNN:
+class TextCNN(object):
+    """
+    A CNN for text classification.
+    Uses an embedding layer, followed by a convolutional, max-pooling and softmax layer.
+    """
 
     def __init__(
             self, sequence_length, num_classes, vocab_size,
             embedding_size, filter_sizes, num_filters, l2_reg_lambda=0.0):
 
-        # sequence_length     최대 문자열 길이
-        # num_classes         긍정 / 부정 (2)
-        # vocab_size          총 단어 개수
-        # embedding_size      문자 임베딩 최대 길이(128)
-        # filter_sizes        (3, 4, 5)
-        # num_filters         크기 당 필터 개수(128)
-        # l2_reg_lambda       l2 정규화 (건드릴 일 없음)
-
+        # Placeholders for input, output and dropout
         self.input_x = tf.placeholder(
             tf.int32, [None, sequence_length], name="input_x")
         self.input_y = tf.placeholder(
@@ -27,17 +20,23 @@ class TextCNN:
         self.dropout_keep_prob = tf.placeholder(
             tf.float32, name="dropout_keep_prob")
 
-        with tf.device('/gpu:0'), tf.name_scope("embedding"):
-            self.W = tf.Variable(tf.random_uniform(
-                [vocab_size, embedding_size], -1.0, 1.0), name='W')
-            self.embedded_chars = tf.nn.embedding_lookup
+        # Keeping track of l2 regularization loss (optional)
+        l2_loss = tf.constant(0.0)
+
+        # Embedding layer
+        with tf.device('/cpu:0'), tf.name_scope("embedding"):
+            self.W = tf.Variable(
+                tf.random_uniform([vocab_size, embedding_size], -1.0, 1.0),
+                name="W")
+            self.embedded_chars = tf.nn.embedding_lookup(self.W, self.input_x)
             self.embedded_chars_expanded = tf.expand_dims(
                 self.embedded_chars, -1)
 
+        # Create a convolution + maxpool layer for each filter size
         pooled_outputs = []
-
-        for i, filter_size in enumerate(filter_sizes):
+        for _, filter_size in enumerate(filter_sizes):
             with tf.name_scope("conv-maxpool-%s" % filter_size):
+                # Convolution Layer
                 filter_shape = [filter_size, embedding_size, 1, num_filters]
                 W = tf.Variable(tf.truncated_normal(
                     filter_shape, stddev=0.1), name="W")
@@ -47,29 +46,31 @@ class TextCNN:
                     self.embedded_chars_expanded,
                     W,
                     strides=[1, 1, 1, 1],
-                    padding="VALID"
-                )
-
-                h = tf.nn.relu(tf.nn.bias_add(conv, b))
-
-                # 이거 아직 이해 못함 ㅠㅠ
+                    padding="VALID",
+                    name="conv")
+                # Apply nonlinearity
+                h = tf.nn.relu(tf.nn.bias_add(conv, b), name="relu")
+                # Maxpooling over the outputs
+                # TODO: 여기부터 아직 이해 못함 ㅠ
                 pooled = tf.nn.max_pool(
                     h,
                     ksize=[1, sequence_length - filter_size + 1, 1, 1],
                     strides=[1, 1, 1, 1],
-                    padding='VALID'
-                )
+                    padding='VALID',
+                    name="pool")
                 pooled_outputs.append(pooled)
 
+        # Combine all the pooled features
         num_filters_total = num_filters * len(filter_sizes)
         self.h_pool = tf.concat(pooled_outputs, 3)
         self.h_pool_flat = tf.reshape(self.h_pool, [-1, num_filters_total])
 
+        # Add dropout
         with tf.name_scope("dropout"):
             self.h_drop = tf.nn.dropout(
                 self.h_pool_flat, self.dropout_keep_prob)
 
-        # 여기서부터 그냥 복붙함
+        # Final (unnormalized) scores and predictions
         with tf.name_scope("output"):
             W = tf.get_variable(
                 "W",
