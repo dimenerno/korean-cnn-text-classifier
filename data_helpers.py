9931a973888d0735edf8d23cf118a72882a2f440
ssy05468 <36706193+ssy05468@users.noreply.github.com>
2018-12-16 00:54:20 +0900

    Update data_helpers.py

--- data_helpers.py
+++ data_helpers.py
@@ -3,7 +3,7 @@
 
 import numpy as np
 import re
-from konlpy.tag import Kkma
+#from konlpy.tag import Kkma
 
 
 def clean_str(string):
@@ -11,13 +11,6 @@ def clean_str(string):
     Tokenization/string cleaning for all datasets except for SST.
     Original taken from https://github.com/yoonkim/CNN_sentence/blob/master/process_data.py
     """
-    # string = re.sub(r"[^A-Za-z0-9(),!?\'\`]", " ", string)
-    # string = re.sub(r"\'s", " \'s", string)
-    # string = re.sub(r"\'ve", " \'ve", string)
-    # string = re.sub(r"n\'t", " n\'t", string)
-    # string = re.sub(r"\'re", " \'re", string)
-    # string = re.sub(r"\'d", " \'d", string)
-    # string = re.sub(r"\'ll", " \'ll", string)
     string = re.sub(r"[^ ㄱ-ㅣ가-힣ㅋㅎ\.\^0-9(),!?\'\"♥]", " ", string)
     string = re.sub(r"[ㄱ-ㅊㅌ-ㅍㅏ-ㅛㅣ]", "", string)  # ㅋ ㅎ ㅜ ㅠ ㅡ 빼고 제거
     string = re.sub(r"\.", " . ", string)
@@ -35,16 +28,16 @@ def split_func(iterator):
     return (x.split() for x in iterator)
 
 
-def tokenize(string):
-    """
-    형태소 분석
-    "나는 너가 좋아" -> "나 는 너 가 좋아"
-    """
-    kkma = Kkma()
-    tokenized_string = ""
-    for i in kkma.morphs(string):
-        tokenized_string += i + ' '
-    return tokenized_string
+# def tokenize(string):
+#     """
+#     형태소 분석
+#     "나는 너가 좋아" -> "나 는 너 가 좋아"
+#     """
+#     kkma = Kkma()
+#     tokenized_string = ""
+#     for i in kkma.morphs(string):
+#         tokenized_string += i + ' '
+#     return tokenized_string
 
 
 def load_data_and_labels(positive_data_file, negative_data_file):
@@ -55,23 +48,26 @@ def load_data_and_labels(positive_data_file, negative_data_file):
     # Load data from files
     positive_examples = list(
         open(positive_data_file, "r", encoding='utf-8').readlines())
+    positive_examples = [s.strip() for s in positive_examples]
     negative_examples = list(
         open(negative_data_file, "r", encoding='utf-8').readlines())
+    negative_examples = [s.strip() for s in negative_examples]
 
+    # positive_examples = [s.strip() for s in positive_examples]
+    # negative_examples = [s.strip() for s in negative_examples]
+    
+    
     # positive_tagged = open("./tagged_data/positive.txt", 'w', encoding="utf-8")
     # negative_tagged = open("./tagged_data/negative.txt", 'w', encoding="utf-8")
-
+    
     # for s in positive_examples:
     #     positive_tagged.write(tokenize(clean_str(s.strip())) + "\n")
     # for s in negative_examples:
     #     negative_tagged.write(tokenize(clean_str(s.strip())) + "\n")
-
-    # positive_examples = [s.strip() for s in positive_examples]
-
-    # negative_examples = [s.strip() for s in negative_examples]
+    
     # Split by words
     x_text = positive_examples + negative_examples
-    # x_text = [tokenize(clean_str(sent)) for sent in x_text]
+    x_text = [clean_str(sent)) for sent in x_text]
 
     positive_labels = [[0, 1] for _ in positive_examples]
     negative_labels = [[1, 0] for _ in negative_examples]
