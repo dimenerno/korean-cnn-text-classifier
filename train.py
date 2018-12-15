72e7aacd992afa4cf56328e58db0af507340b135
ssy05468 <36706193+ssy05468@users.noreply.github.com>
2018-12-15 22:20:04 +0900

    Update train.py

--- train.py
+++ train.py
@@ -25,7 +25,6 @@ tf.flags.DEFINE_string("filter_sizes","3,4,5","Comma-separated filter sizes (def
 tf.flags.DEFINE_integer("num_filters", 128, "Number of filters per filter size (default: 128)")
 tf.flags.DEFINE_float("dropout_keep_prob", 0.5, "Dropout keep probability (default: 0.5)")
 tf.flags.DEFINE_float("l2_reg_lambda", 0.0, "L2 regularization lambda (default: 0.0)")
-tf.flags.DEFINE_string("gpu","/gpu:6","using gpu device")
 
 # Training parameters
 tf.flags.DEFINE_integer("batch_size", 64, "Batch Size (default: 64)")
@@ -184,7 +183,7 @@ def train(x_train, y_train, vocab_processor, x_dev, y_dev):
                 list(zip(x_train, y_train)), FLAGS.batch_size, FLAGS.num_epochs)
 
             # Training loop. For each batch...
-            with tf.device(FLAGS.gpu):
+            with tf.device('/gpu:6'):
                 for batch in batches:
                     x_batch, y_batch = zip(*batch)
                     train_step(x_batch, y_batch)
